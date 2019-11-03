7d36c242c74c5f955ff1245da1f99817068c8457
Gonzapepe <addamogonzalo@gmail.com>
2019-11-03 15:32:38 -0300

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Lucas\Desktop\python project\django\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
